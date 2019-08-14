cd21d13aa1024e375766452f4e46d7c6f2253e7c
Yi Zhang <cattyzhang0110@gmail.com>
2019-08-14 22:09:38 +1000

    Initial Commit

---
+++ YiZhang-iOSApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YiZhang-iOSApplication
+//
+//  Created by Yi Zhang on 14/8/19.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
